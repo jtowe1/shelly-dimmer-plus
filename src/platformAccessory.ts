97a0047379faf7e66828f7be9436d60f9a115d2d
Jeremiah <jeremiah.towe@gmail.com>
2023-09-24 14:07:17 -0400

    Cleanup and getBrightness

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -12,15 +12,6 @@ import { ExampleHomebridgePlatform } from './platform';
 export class ExamplePlatformAccessory {
   private service: Service;
 
-  /**
-   * These are just used to create a working example
-   * You should implement your own code to track the state of your accessory
-   */
-  private exampleStates = {
-    On: false,
-    Brightness: 100,
-  };
-
   constructor(
     private readonly platform: ExampleHomebridgePlatform,
     private readonly accessory: PlatformAccessory,
@@ -50,93 +41,28 @@ export class ExamplePlatformAccessory {
 
     // register handlers for the Brightness Characteristic
     this.service.getCharacteristic(this.platform.Characteristic.Brightness)
-      .onSet(this.setBrightness.bind(this));       // SET - bind to the 'setBrightness` method below
-
-    /**
-     * Creating multiple services of the same type.
-     *
-     * To avoid "Cannot add a Service with the same UUID another Service without also defining a unique 'subtype' property." error,
-     * when creating multiple services of the same type, you need to use the following syntax to specify a name and subtype id:
-     * this.accessory.getService('NAME') || this.accessory.addService(this.platform.Service.Lightbulb, 'NAME', 'USER_DEFINED_SUBTYPE_ID');
-     *
-     * The USER_DEFINED_SUBTYPE must be unique to the platform accessory (if you platform exposes multiple accessories, each accessory
-     * can use the same sub type id.)
-     */
-
-    // Example: add two "motion sensor" services to the accessory
-    // const motionSensorOneService = this.accessory.getService('Motion Sensor One Name') ||
-    //   this.accessory.addService(this.platform.Service.MotionSensor, 'Motion Sensor One Name', 'YourUniqueIdentifier-1');
-
-    // const motionSensorTwoService = this.accessory.getService('Motion Sensor Two Name') ||
-    //   this.accessory.addService(this.platform.Service.MotionSensor, 'Motion Sensor Two Name', 'YourUniqueIdentifier-2');
-
-    // /**
-    //  * Updating characteristics values asynchronously.
-    //  *
-    //  * Example showing how to update the state of a Characteristic asynchronously instead
-    //  * of using the `on('get')` handlers.
-    //  * Here we change update the motion sensor trigger states on and off every 10 seconds
-    //  * the `updateCharacteristic` method.
-    //  *
-    //  */
-    // let motionDetected = false;
-    // setInterval(() => {
-    //   // EXAMPLE - inverse the trigger
-    //   motionDetected = !motionDetected;
-
-    //   // push the new value to HomeKit
-    //   motionSensorOneService.updateCharacteristic(this.platform.Characteristic.MotionDetected, motionDetected);
-    //   motionSensorTwoService.updateCharacteristic(this.platform.Characteristic.MotionDetected, !motionDetected);
-
-    //   this.platform.log.debug('Triggering motionSensorOneService:', motionDetected);
-    //   this.platform.log.debug('Triggering motionSensorTwoService:', !motionDetected);
-    // }, 10000);
+      .onSet(this.setBrightness.bind(this))    // SET - bind to the 'setBrightness` method below
+      .onGet(this.getBrightness.bind(this));   // GET - bind to the 'getBrightness` method below
   }
 
-  /**
-   * Handle "SET" requests from HomeKit
-   * These are sent when the user changes the state of an accessory, for example, turning on a Light bulb.
-   */
   async setOn(value: CharacteristicValue) {
-    // implement your own code to turn your device on/off
-    this.exampleStates.On = value as boolean;
-    const url = `http://10.0.0.151/rpc/Light.Set?id=0&on=${this.exampleStates.On}`;
+    const url = `http://10.0.0.151/rpc/Light.Set?id=0&on=${value as boolean}`;
     try {
       const response = await axios.get(url);
       this.platform.log.debug('response: ', response.data);
     } catch (error) {
       this.platform.log.debug('error: ', error);
+      throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
     }
-
-    // if (device instanceof ShellyPlus1) {
-    //     const plus1 = device as ShellyPlus1;
-
-    //     // toggle the switch
-    //     await plus1.switch0.toggle();
 
     this.platform.log.debug('Set Characteristic On ->', value);
   }
 
-  /**
-   * Handle the "GET" requests from HomeKit
-   * These are sent when HomeKit wants to know the current state of the accessory, for example, checking if a Light bulb is on.
-   *
-   * GET requests should return as fast as possbile. A long delay here will result in
-   * HomeKit being unresponsive and a bad user experience in general.
-   *
-   * If your device takes time to respond you should update the status of your device
-   * asynchronously instead using the `updateCharacteristic` method instead.
 
-   * @example
-   * this.service.updateCharacteristic(this.platform.Characteristic.On, true)
-   */
   async getOn(): Promise<CharacteristicValue> {
-    // implement your own code to check if the device is on
-    // const isOn = this.exampleStates.On;
     let isOn;
 
     const url = 'http://10.0.0.151/rpc/Light.GetStatus?id=0';
-
 
     try {
       const response = await axios.get(url);
@@ -144,26 +70,44 @@ export class ExamplePlatformAccessory {
       isOn = response.data.output;
     } catch (error) {
       this.platform.log.debug('error: ', error);
+      throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
     }
 
-
     this.platform.log.debug('Get Characteristic On ->', isOn);
-
-    // if you need to return an error to show the device as "Not Responding" in the Home app:
-    // throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
 
     return isOn;
   }
 
-  /**
-   * Handle "SET" requests from HomeKit
-   * These are sent when the user changes the state of an accessory, for example, changing the Brightness
-   */
   async setBrightness(value: CharacteristicValue) {
-    // implement your own code to set the brightness
-    this.exampleStates.Brightness = value as number;
+    const url = `http://10.0.0.151/rpc/Light.Set?id=0&brightness=${value as number}`;
+    try {
+      const response = await axios.get(url);
+      this.platform.log.debug('response: ', response.data);
+    } catch (error) {
+      this.platform.log.debug('error: ', error);
+      throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
+    }
 
     this.platform.log.debug('Set Characteristic Brightness -> ', value);
+  }
+
+  async getBrightness(): Promise<CharacteristicValue> {
+    let brightness;
+
+    const url = 'http://10.0.0.151/rpc/Light.GetStatus?id=0';
+
+    try {
+      const response = await axios.get(url);
+      this.platform.log.debug('response: ', response.data);
+      brightness = response.data.brightness;
+    } catch (error) {
+      this.platform.log.debug('error: ', error);
+      throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
+    }
+
+    this.platform.log.debug('Get Characteristic On ->', brightness);
+
+    return brightness;
   }
 
 }
