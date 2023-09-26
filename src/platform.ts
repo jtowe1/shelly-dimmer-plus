a9ce6ea3731375116140796b513ccab3dbf71b9c
Jeremiah <jeremiah.towe@gmail.com>
2023-09-26 23:55:12 +0000

    Update logging

--- src/platform.ts
+++ src/platform.ts
@@ -62,131 +62,69 @@ export class ShellyDimmerPlusPlatform implements DynamicPlatformPlugin {
         let deviceId: string | null = null;
         let ipAddress: string | null = null;
 
-        const devices: IShellyDimmerPlus[] = [];
-
-        this.log.debug('setting up mDNS');
-    this.mdns!.on('response', (response) => {
-        for (const answer of response.answers) {
-            if (answer.type === 'PTR' && answer.name === SERVICE_NAME && answer.data) {
-                this.log.debug('found a shelly device', response);
-                deviceId = answer.data.split('.', 1)[0];
+        this.log.info('setting up mDNS');
+        this.mdns!.on('response', (response) => {
+            for (const answer of response.answers) {
+                if (answer.type === 'PTR' && answer.name === SERVICE_NAME && answer.data) {
+                    this.log.debug('Found a shelly device', response);
+                    deviceId = answer.data.split('.', 1)[0];
+                }
             }
-        }
 
-        for (const additionals of response.additionals) {
-            if (additionals.type === 'A') {
-                ipAddress = additionals.data;
+            for (const additionals of response.additionals) {
+                if (additionals.type === 'A') {
+                    ipAddress = additionals.data;
+                }
             }
-        }
 
-        if (deviceId && ipAddress) {
-            this.log.debug('Getting device info');
-            const url = 'http://10.0.0.151/rpc/Shelly.GetDeviceInfo';
-            axios.get(url)
-                .then((deviceResponse) => {
-                    this.log.debug('Got info ', deviceResponse.data);
-                    if (deviceResponse.data.model === MODEL) {
-                        this.log.debug('Found a dimmer plus, stopping mDNS');
-                        const device = {
-                            name: deviceResponse.data.name,
-                            id: deviceResponse.data.id,
-                            mac: deviceResponse.data.mac,
-                            model: deviceResponse.data.model,
-                            gen: deviceResponse.data.gen,
-                            fw_id: deviceResponse.data.fw_id,
-                            ver: deviceResponse.data.ver,
-                            app: deviceResponse.data.app,
-                            auth_en: deviceResponse.data.auth_en,
-                        } as IShellyDimmerPlus;
-                        devices.push(device);
-                        this.log.debug('devices', devices);
-              this.mdns!.destroy();
-              for (const device of devices) {
-                  const uuid = this.api.hap.uuid.generate(device.id);
-                  const existingAccessory = this.accessories.find(accessory => accessory.UUID === uuid);
-                  if (existingAccessory) {
-                      this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
-                      new ShellyDimmerPlusAccessory(this, existingAccessory);
-                  } else {
-                      this.log.info('Adding new accessory:', device.name);
-                      const accessory = new this.api.platformAccessory(device.name, uuid);
-                      accessory.context.device = device;
-                      new ShellyDimmerPlusAccessory(this, accessory);
-                      this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
-                  }
-              }
-                    }
-                });
-        }
-    });
+            if (deviceId && ipAddress) {
+                this.log.info('Getting Shelly device info');
+                const url = `http://${ipAddress}/rpc/Shelly.GetDeviceInfo`;
+                axios.get(url)
+                    .then((deviceResponse) => {
+                        if (deviceResponse.data.model === MODEL) {
+                            this.log.info('Device has info: ', deviceResponse.data);
+                            this.log.info('Found a dimmer plus, stopping mDNS');
+                            const device = {
+                                name: deviceResponse.data.name,
+                                id: deviceResponse.data.id,
+                                mac: deviceResponse.data.mac,
+                                model: deviceResponse.data.model,
+                                gen: deviceResponse.data.gen,
+                                fw_id: deviceResponse.data.fw_id,
+                                ver: deviceResponse.data.ver,
+                                app: deviceResponse.data.app,
+                                auth_en: deviceResponse.data.auth_en,
+                            } as IShellyDimmerPlus;
 
-    this.log.debug('querying mDNS');
-    this.mdns!.query(SERVICE_NAME, 'PTR', (error: Error | null) => {
-        if (error) {
-            this.log.error('error while querying:', error);
-        }
-    });
+                            this.mdns!.destroy();
 
-        // EXAMPLE ONLY
-        // A real plugin you would discover accessories from the local network, cloud services
-        // or a user-defined array in the platform config.
-        // const exampleDevices = [
-        //   {
-        //     exampleUniqueId: 'ABCD',
-        //     exampleDisplayName: 'Bedroom',
-        //   },
-        //   {
-        //     exampleUniqueId: 'EFGH',
-        //     exampleDisplayName: 'Kitchen',
-        //   },
-        // ];
+                            const uuid = this.api.hap.uuid.generate(device.id);
+                            const existingAccessory = this.accessories.find(accessory => accessory.UUID === uuid);
 
-        // loop over the discovered devices and register each one if it has not already been registered
-        // for (const device of devices) {
+                            if (existingAccessory) {
+                                this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
+                                new ShellyDimmerPlusAccessory(this, existingAccessory);
+                            } else {
+                                this.log.info('Adding new accessory:', device.name);
+                                const accessory = new this.api.platformAccessory(device.name, uuid);
+                                accessory.context.device = device;
+                                new ShellyDimmerPlusAccessory(this, accessory);
+                                this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
+                            }
 
-        //   // generate a unique id for the accessory this should be generated from
-        //   // something globally unique, but constant, for example, the device serial
-        //   // number or MAC address
-        //   // const uuid = this.api.hap.uuid.generate(device.exampleUniqueId);
+                        } else {
+                            this.log.info(`Device ${deviceResponse.data.model} is not a dimmer plus, ignoring`);
+                        }
+                    });
+            }
+        });
 
-        //   // see if an accessory with the same uuid has already been registered and restored from
-        //   // the cached devices we stored in the `configureAccessory` method above
-        //   const existingAccessory = this.accessories.find(accessory => accessory.UUID === device.id);
-
-        //   if (existingAccessory) {
-        //     // the accessory already exists
-        //     this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
-
-        //     // if you need to update the accessory.context then you should run `api.updatePlatformAccessories`. eg.:
-        //     // existingAccessory.context.device = device;
-        //     // this.api.updatePlatformAccessories([existingAccessory]);
-
-        //     // create the accessory handler for the restored accessory
-        //     // this is imported from `platformAccessory.ts`
-        //     new ShellyDimmerPlusAccessory(this, existingAccessory);
-
-        //     // it is possible to remove platform accessories at any time using `api.unregisterPlatformAccessories`, eg.:
-        //     // remove platform accessories when no longer present
-        //     // this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [existingAccessory]);
-        //     // this.log.info('Removing existing accessory from cache:', existingAccessory.displayName);
-        //   } else {
-        //     // the accessory does not yet exist, so we need to create it
-        //     this.log.info('Adding new accessory:', device.exampleDisplayName);
-
-        //     // create a new accessory
-        //     const accessory = new this.api.platformAccessory(device.exampleDisplayName, device.id);
-
-        //     // store a copy of the device object in the `accessory.context`
-        //     // the `context` property can be used to store any data about the accessory you may need
-        //     accessory.context.device = device;
-
-        //     // create the accessory handler for the newly create accessory
-        //     // this is imported from `platformAccessory.ts`
-        //     new ShellyDimmerPlusAccessory(this, accessory);
-
-    //     // link the accessory to your platform
-    //     this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
-    //   }
-    // }
+        this.log.info('querying mDNS');
+        this.mdns!.query(SERVICE_NAME, 'PTR', (error: Error | null) => {
+            if (error) {
+                this.log.error('error while querying:', error);
+            }
+        });
     }
 }
